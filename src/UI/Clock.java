f2d5401ea983dd7d4ab34752dd96718a71a4fd2a
lefterisYA <elefterios.lundell@yaelev.se>
2021-11-30 13:42:52 +0100

    Lade till ascii klocka

--- src/UI/Clock.java
+++ src/UI/Clock.java
@@ -1,5 +1,151 @@
 package UI;
 
-public class Clock {
+import java.util.Calendar;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
+public class Clock {
+	final static private String[][] asciiDgts = {
+			{
+				"  ████  " ,
+				" ██  ██ " ,
+				" ██  ██ " ,
+				" ██  ██ " ,
+				"  ████  "
+			},
+			{
+				" ████   " ,
+				"   ██   " ,
+				"   ██   " ,
+				"   ██   " ,
+				" ██████ "
+			},
+			{
+				"  ████  " ,
+				" ██  ██ " ,
+				"    ██  " ,
+				"   ██   " ,
+				" ██████ "
+			},
+			{
+				"  ████  " ,
+				" ██  ██ " ,
+				"    ██  " ,
+				" ██  ██ " ,
+				"  ████  "
+			},
+			{
+				" ██  ██ " ,
+				" ██  ██ " ,
+				" ██████ " ,
+				"     ██ " ,
+				"     ██ "
+			},
+			{
+				" ██████ " ,
+				" ██     " ,
+				" █████  " ,
+				"     ██ " ,
+				" █████  "
+			},
+			{
+				"  ████  " ,
+				" ██     " ,
+				" █████  " ,
+				" ██  ██ " ,
+				"  ████  "
+			},
+			{
+				" ██████ " ,
+				"    ██  " ,
+				"   ██   " ,
+				"  ██    " ,
+				" ██     "
+			},
+			{
+				"  ████  " ,
+				" ██  ██ " ,
+				"  ████  " ,
+				" ██  ██ " ,
+				"  ████  "
+			},
+			{
+				"  ████  " ,
+				" ██  ██ " ,
+				"  █████ " ,
+				"     ██ " ,
+				"  ████  "
+			},
+			{
+				"    " ,
+				" ██ " ,
+				"    " ,
+				" ██ " ,
+				"    "
+			}
+	};
+	
+	static public void print(int hour, int mins, int secs) {
+		System.out.println(gnrtAsciiChars(hour, mins, secs));
+	}
+	
+	static public void prntCurrTime() {
+		Calendar cdr = Calendar.getInstance(); // creates a new calendar instance
+		int hour = cdr.get(Calendar.HOUR_OF_DAY);
+		int mins = cdr.get(Calendar.MINUTE);
+		int secs = cdr.get(Calendar.SECOND);
+
+		System.out.println(gnrtAsciiChars(hour, mins, secs));
+	}
+
+	static private String gnrtAsciiChars(int hour, int mins, int secs) {
+		String rtrnValu = "";
+
+		int[] sprtDgtsH = getSprtDgts(hour);
+		int[] sprtDgtsM = getSprtDgts(mins);
+		int[] sprtDgtsS = getSprtDgts(secs);
+
+		for ( int row=0; row<asciiDgts[0].length; row++ ) {
+			for ( int i=0; i<sprtDgtsH.length; i++ ) {
+				rtrnValu += asciiDgts[sprtDgtsH[i]][row];
+			}
+			rtrnValu += asciiDgts[10][row]; // Column
+			for ( int i=0; i<sprtDgtsM.length; i++ ) {
+				rtrnValu += asciiDgts[sprtDgtsM[i]][row];
+			}
+			rtrnValu += asciiDgts[10][row]; // Column
+			for ( int i=0; i<sprtDgtsS.length; i++ ) {
+				rtrnValu += asciiDgts[sprtDgtsS[i]][row];
+			}
+			rtrnValu += "\n";
+		}
+		return rtrnValu;
+	}
+	
+	static private int[] getSprtDgts(int num) {
+		int dgtsLen = (int) ( Math.log10(num) + 1 );
+
+		// Since we fill in the digits from the right, if we simply do length++, then the leftmost digit will get the
+		// value 0, so it's padded correctly.
+		if ( dgtsLen == 1 )
+			dgtsLen++;
+		
+		int[] sprtDgts = new int[dgtsLen];
+		for ( int i=0; i<dgtsLen; i++ ) {
+			sprtDgts[dgtsLen-i-1] = (num % 10);
+			num=num/10;
+		}
+		return sprtDgts;
+	}
+
+	public void run() {
+	    ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
+	    exec.scheduleAtFixedRate(new Runnable() {
+	    	@Override
+	    	public void run() {
+	    		prntCurrTime();
+	    	}
+	    }, 0, 1, TimeUnit.SECONDS);
+	}
 }
--- src/test/Test.java
+++ src/test/Test.java
@@ -1,7 +1,18 @@
 package test;
 
+import UI.Clock;
+
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("här kan man göra massa tester");
+		
+		Clock.print(1, 55, 1);
+		Clock.print(15, 5, 1);
+		Clock.print(15, 45, 60);
+		
+		Clock.prntCurrTime();
+		
+		Clock clck = new Clock();
+		clck.run();
 	}
 }
