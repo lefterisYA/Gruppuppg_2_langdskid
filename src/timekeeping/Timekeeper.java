47bc9fee1b7c8761e6b9f74668b6f6760f5b083d
lefterisYA <elefterios.lundell@yaelev.se>
2022-01-02 14:50:52 +0100

    Mergar utvalda från better-timekeeping

---
+++ src/timekeeping/Timekeeper.java
@@ -0,0 +1,65 @@
+package timekeeping;
+
+// Här finns metoder för att sätta starttid, mellantid och sluttid för aktuell åkare, använd timeKeeper till gui
+public class Timekeeper {
+		private Time startTime;
+		private Time checkPointTime;
+		private Time finishTime;
+		
+		public Timekeeper() {
+			this.startTime = new Time();
+		}
+
+		public Timekeeper(Time startTime) {
+			this.startTime = startTime;
+		}
+		
+		public Time getStartTime() {
+			return startTime;
+		}
+		
+		public void setStartTime(Time startTime) {
+			this.startTime = startTime;
+		}
+		
+		public Time getFinishTime() {
+			return finishTime;
+		}
+
+		public void setFinishTime(Time finishTime) {
+			this.finishTime = finishTime;
+		}
+		
+		public Time getCheckPointTime() {
+			return checkPointTime;
+		}
+		public void setCheckPointTime(Time checkPointTime) {
+			this.checkPointTime = checkPointTime;
+		}
+		
+		public Time getRunningTimeToFinish() {
+			return finishTime.diffTo(startTime);
+		}
+
+		public Time getRunningTimeToCheckpoint() {
+			return checkPointTime.diffTo(startTime);
+		}
+
+//		public void setCheckPointTime() { // denna ska du ropa på Joakim
+//			long now = System.currentTimeMillis(); // tiden nu i millisekunder		
+//			int difference = (int) (now-this.getStartTime())/1000; // tar skillnaden mellan aktuell start tid och nu tid
+//			String diffString = TimeUtils.toString(TimeUtils.getInstance().timeConverter(difference)); // gör om det till timmar minuter sekunder
+//			this.setCheckPointTime(diffString); // mellantiden som åkaren fick lagrar vi fast i variablen checkPointTime, varje åkare kommer på sin klocka ha en lagrad mellantid
+//			return diffString;
+//		}
+//		public void setFinishTime() { // Sätter sluttid
+//			long now = System.currentTimeMillis(); // tiden nu i millisekunder
+//			int difference = (int) (now-this.getStartTime())/1000; // tar skillnaden mellan aktuell start tid och nu tid
+//			String diffString = TimeUtils.toString(TimeUtils.getInstance().timeConverter(difference)); // gör om det till timmar minuter sekunder
+//			this.setFinishTime(diffString);
+//			this.setFinishTimeMillis(difference);
+//			return diffString;
+//		}
+		
+
+	}
